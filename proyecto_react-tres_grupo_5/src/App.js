39296bb3d54d8896814884f8d5f4b1896902c085
Christian Gutierrez <Wearelectric93@gmail.com>
2022-10-07 15:06:43 -0300

    Add: rules.GutierrezChristian

--- proyecto_react-tres_grupo_5/src/App.js
+++ proyecto_react-tres_grupo_5/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Reglas from './components/Reglas';
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+        <Route path="/Reglas" element={<Reglas />} />
         </Routes>
       </BrowserRouter>
     </>
