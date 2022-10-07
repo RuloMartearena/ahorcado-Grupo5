e937ec3ea8940ab33561bbf8519f0c7fdbfc7f51
RuloMartearena <93354080+RuloMartearena@users.noreply.github.com>
2022-10-07 15:48:32 -0300

    Fix: css styles. Matias Martearena

--- proyecto_react-tres_grupo_5/src/components/Recordatorio.js
+++ proyecto_react-tres_grupo_5/src/components/Recordatorio.js
@@ -1,7 +1,5 @@
 import '../styles/recordatorio.css';
 
-
-
 export default function Recordatorio() {
     return (
         <section className='section__recordatorio-conteiner'>
@@ -14,22 +12,15 @@ export default function Recordatorio() {
                     <li className='li__recordatorio'>No puedes equivocarte más de 5 veces</li>
                     <li className='li__recordatorio'>Si completas las palabras, GANARAS!</li>
                 </ul>
-                
             </div>
-                <div>
-                    <li className='ul__botones'>
-                        <a href="/"> 
-                            <button className='button__rec'>Home</button>
-                        </a>                        
-                    </li>
-                    <li className='ul__botones'>
-                        <a href="/Juego"> 
-                            <button className='button__rec'>Juego</button>
-                        </a> 
-                    </li>
-                    
-                </div>
-                
+            <div className='divbutton-conteiner'>
+                <a href="/">
+                    <button className='button__return'>Home</button>
+                </a>
+                <a href="/Juego">
+                    <button className='button__return'>Juego</button>
+                </a>
+            </div>
         </section>
     )
 }
