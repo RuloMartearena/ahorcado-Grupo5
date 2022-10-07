e19427c52b341053cd789ecfc24cf333eeca128c
TitiBeguier <beguiercartalibro@gmail.com>
2022-10-07 15:11:05 -0300

    Add: developers page. Beguier-Ignacio

--- proyecto_react-tres_grupo_5/src/App.js
+++ proyecto_react-tres_grupo_5/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Desarrolladores from './components/Desarrolladores';
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+        <Route path="/Desarrolladores" element={<Desarrolladores />} />
         </Routes>
       </BrowserRouter>
     </>
